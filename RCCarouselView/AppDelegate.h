c1a42d5228a42d81064c8ef84a2275d99777ae7e
rongchen <rongchen@pwrd.com>
2018-10-23 13:58:11 +0800

    Initial Commit

---
+++ RCCarouselView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RCCarouselView
+//
+//  Created by rong on 2018/10/23.
+//  Copyright © 2018 rong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RCCarouselView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  RCCarouselView
+//
+//  Created by rong on 2018/10/23.
+//  Copyright © 2018 rong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
